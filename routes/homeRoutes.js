530b3cd909eb97ec3873bee5678021486ffeeb20
galvin-SH <mgalvin1993@gmail.com>
2023-10-04 23:21:55 -0700

    added check for user login status before they can enter the dashboard view. added some misc comments

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth')
 const { renderHomePage, renderDashboard, renderLoginPage } = require('../controllers/homeControllers')
 
+// get request to '/' will use renderHomepage for the view
 router.route('/')
     .get(renderHomePage);
 
+// get request to '/dashboard' will use renderDashboard for the view if withAuth passes, otherwise withAuth will redirect to the login page
 router.route('/dashboard')
-    .get(renderDashboard);
+    .get(withAuth, renderDashboard);
 
+//get request to '/login' will use renderLoginPage for the view
 router.route('/login')
     .get(renderLoginPage);
-
 
 module.exports = router;
--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,9 @@
+// If the session.loggedIn value is false then the user will be redirected to the login page
+
 const withAuth = (req, res, next) => {
     if (!req.session.loggedIn) {
+        console.log('\nloggedIn is false');
+        console.log('redirecting user to login page\n');
         res.redirect('/login');
     } else { next()};
 };
