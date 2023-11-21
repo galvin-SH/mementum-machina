0eda83045592d831e8fde7b3ccdf8a81bf686c99
galvin-SH <mgalvin1993@gmail.com>
2023-11-21 13:02:25 -0800

    heroku troubleshooting

--- utils/connection.js
+++ utils/connection.js
@@ -3,7 +3,14 @@ const Sequelize = require('sequelize');
 let sequelize;
 
 if (process.env.JSAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JSAWSDB_URL);
+    sequelize = new Sequelize({
+        host: 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        database: 'vkz6kr4to20jhfof',
+        username: 'jaurruzde8h6mpo2',
+        password: 'lffnhknqfcnhlxdz',
+        dialect: 'mysql',
+        port: 3306,
+    });
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
