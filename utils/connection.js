1a34387065a7b32a9dce514f4bf485f493bd6e0e
galvin-SH <mgalvin1993@gmail.com>
2023-11-21 13:32:12 -0800

    fixed db connection

--- utils/connection.js
+++ utils/connection.js
@@ -2,15 +2,8 @@ const Sequelize = require('sequelize');
 
 let sequelize;
 
-if (process.env.JSAWSDB_URL) {
-    sequelize = new Sequelize({
-        host: 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        database: 'vkz6kr4to20jhfof',
-        username: 'jaurruzde8h6mpo2',
-        password: 'lffnhknqfcnhlxdz',
-        dialect: 'mysql',
-        port: 3306,
-    });
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
